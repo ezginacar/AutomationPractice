c6beb867f7931bf5014a539584bdec614af68e2f
Ezgi Nacar <ezgi.nacar@finartz.com>
2021-04-11 15:13:09 +0300

    Other.java added

---
+++ src/test/java/product/Other.java
@@ -0,0 +1,34 @@
+package product;
+
+public class Other implements Product{
+    private String name;
+    private double price;
+    @Override
+    public void name(String productName) {
+        this.name = productName;
+    }
+
+    @Override
+    public void price(double productPrice) {
+        this.price = productPrice;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public double getPrice() {
+        return price;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Search product info ->{" +
+                "name='" + name + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
