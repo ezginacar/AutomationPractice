7054257c318d187e7fe9e3acc247eb2a394c7a7e
ezginacar <ezgi.nacar@finartz.com>
2021-04-21 21:40:15 +0300

    TestRunner.java updated

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -1,15 +1,16 @@
 package runner;
 
 
+import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
 
+@RunWith(Cucumber.class)
 @CucumberOptions(
         monochrome = true,
         features = "src/test/resources/features",
-        glue = {"src/test/java/stepDefinitions"},
-        plugin = "json:target/cucumber-reports/CucumberTestReport.json",
-        dryRun = false
-
+        glue = {"stepDefinitions","base"},
+        tags = "@test"
         )
 
 public class TestRunner {
